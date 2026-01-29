701e64e36628a9cb1a2ca4d995b00801816ae746
AdityaGowari22 <nkadityagowari@gmail.com>
2026-01-29 08:25:22 +0530

    Complex class implementations

---
+++ Complex_class_assignment/complexClass.cpp
@@ -0,0 +1,86 @@
+#include "complexClass_header.hpp"
+#include <iostream>
+
+Complex::Complex()
+{
+    real = 0;
+    imag = 0;
+}
+
+// Parameterized Constructor
+Complex::Complex(float r, float i)
+    {
+        real = r;
+        imag = i;
+    }
+
+// Display function
+void Complex::display()
+    {
+        std::cout << real << " + " << imag << "i" << std::endl;
+    }
+
+// Getter for real part
+float Complex::getReal()
+    {
+        return real;
+    }
+
+// Getter for imaginary part
+float Complex::getImag()
+    {
+        return imag;
+    }
+
+// Addition
+Complex Complex::add(Complex obj)
+    {
+        Complex temp;
+        temp.real = real + obj.real;
+        temp.imag = imag + obj.imag;
+        return temp;
+    }
+
+// Subtraction
+Complex Complex::subtract(Complex obj)
+    {
+        Complex temp;
+        temp.real = real - obj.real;
+        temp.imag = imag - obj.imag;
+        return temp;
+    }
+
+// Multiplication
+Complex Complex::multiply(Complex obj)
+    {
+        Complex temp;
+        temp.real = (real * obj.real) - (imag * obj.imag);
+        temp.imag = (real * obj.imag) + (imag * obj.real);
+        return temp;
+    }
+
+// Division
+Complex Complex::divide(Complex obj)
+    {
+        Complex temp;
+        float denominator = (obj.real * obj.real) + (obj.imag * obj.imag);
+        temp.real = ((real * obj.real) + (imag * obj.imag)) / denominator;
+        temp.imag = ((imag * obj.real) - (real * obj.imag)) / denominator;
+        return temp;
+    }
+
+// Conjugate
+Complex Complex::conjugate()
+    {
+        Complex temp;
+        temp.real = real;
+        temp.imag = -imag;
+        return temp;
+    }  
+
+// Norm
+float Complex:: Norm()
+{
+    return sqrt((real * real) + (imag * imag));
+}
+
