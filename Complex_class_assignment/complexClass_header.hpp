ebbd18b3bff6f680ecd3f5fdc1b57cd5ed1515fd
AdityaGowari22 <nkadityagowari@gmail.com>
2026-01-29 08:24:59 +0530

    header file for complex class

---
+++ Complex_class_assignment/complexClass_header.hpp
@@ -0,0 +1,19 @@
+class Complex
+{
+    private:
+        float real;
+        float imag;
+
+    public:
+        Complex();
+        Complex(float r, float i);
+        void display();
+        float getReal();
+        float getImag();
+        Complex add(Complex obj);
+        Complex multiply(Complex obj);
+        Complex subtract(Complex obj);
+        Complex divide(Complex obj);
+        Complex conjugate();
+        float Norm();
+};
